536073c1098228472de2a3d3e42a5013815d33fc
Nekonime01 <angelispretty11@gmail.com>
2025-05-30 23:10:12 +0800

    boholst

---
+++ instructor_logout.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+require_once 'db.php';
+
+if (isset($_SESSION['instructor_id'])) {
+    add_log_entry($_SESSION['instructor_id'], 'instructor', 'logout', $_SERVER['REMOTE_ADDR'], $_SERVER['HTTP_USER_AGENT']);
+}
+
+session_destroy();
+header('Location: instructor_login.php');
+exit();
+?>
